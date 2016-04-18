9644881f0ffe8f170a6d1e8bfa8a9bda0b3dabbb
alexSpinola <alex.spinola.ss@gmail.com>
2016-04-18 22:04:36 +0100

    Commit 4

--- src/dam_ed04_actividad/Empleado.java
+++ src/dam_ed04_actividad/Empleado.java
@@ -10,5 +10,32 @@ package dam_ed04_actividad;
  * @author alexander
  */
 public class Empleado {
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+    private String nombre;
+    private String apellido;
+    private String dni;
     
 }
